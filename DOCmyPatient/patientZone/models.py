19f87567aed9b916fa208a241e0b57a9d85bf231
Julia Cichy <julia.cichyy165@gmail.com>
2019-12-15 16:34:57 +0000

    Adding models

--- DOCmyPatient/patientZone/models.py
+++ DOCmyPatient/patientZone/models.py
@@ -1,2 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Comments(models.Model):
+
+    nurse_comment = models.CharField(max_length=1000)
+    date_posted = models.DateTimeField(default=timezone.now)
+    nurse_name = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nurse_comment
 
--- DOCmyPatient/patientZone/views.py
+++ DOCmyPatient/patientZone/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Comments
 
 def home(request):
-    return render(request, 'docmypatient/patientZone.html')
+    context = {
+        'comments': Comments.objects.all()
+    }
+    return render(request, 'docmypatient/patientZone.html', context)
 
 
+notes = [
+    {
+        'id': 'Notes',
+        'nurse_comment': '10/12/2019',
+        'date_posted': 'Maura Murphy',
+        'nurse_name': 'Today Mary was in good mood and had normal temperature of 37',
+
+
+    },
+    {
+        'title': 'Notes',
+        'date': '11/12/2019',
+        'nurse': 'Maura Murphy',
+        'note': 'Today Mary was feeling unwell, I have her Panadol',
+        'wayne': 'fucking gorgeous'
+
+    }
+]
