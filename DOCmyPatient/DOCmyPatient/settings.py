45c590125c80c7b5fdeb5c9ddeced20822a57beb
Julia Cichy <julia.cichyy165@gmail.com>
2019-12-15 20:54:55 +0000

    adding reg page

--- DOCmyPatient/DOCmyPatient/settings.py
+++ DOCmyPatient/DOCmyPatient/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'patientZone.apps.PatientzoneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
