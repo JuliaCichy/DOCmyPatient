22f8bab5ef90b09983aea4642219e50ca0178033
Julia Cichy <julia.cichyy165@gmail.com>
2020-02-22 23:20:35 +0000

    Update

--- DOCmyPatient/patientZone/views.py
+++ DOCmyPatient/patientZone/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Comments
 
 
@@ -7,3 +7,20 @@ def home(request):
         'comments': Comments.objects.all()
     }
     return render(request, 'docmypatient/patientZone.html', context)
+
+
+def addComment(request):
+    if request == 'POST':
+
+        nurse_comment = request.POST.get('commentData')
+        current_user = request.user
+        comment = Comments(nurse_comment=nurse_comment, nurse_name=current_user)
+
+        if form.is_valid():
+            comment.save()
+            messages.success(request,"Comment added!")
+            return redirect('docmypatient')
+
+    else:
+        return redirect('docmypatient')
+
